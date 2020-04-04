1d24fcaaa3d30ca7c9d03b3906a24c498aca217f
Cindy Chang <cindy@mod-lab.com>
2020-04-04 11:48:20 +0200

    setting up vue press base

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+//essential file for the config of vuepress site
+
+module.exports = {
+  title: 'Bloom Library',
+  description: 'An online library for easy access to components, markup and notes for Front-end design and development.'
+}
